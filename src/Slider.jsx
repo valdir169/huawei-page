b9cd4275410f1a321eb2e7f050952ff0f161d72c
Valdir Contreras <contrerasvaldir06.gmail.com>
2023-07-02 10:52:32 -0500

    add a slider

---
+++ src/Slider.jsx
@@ -0,0 +1,9 @@
+import  './slider.css'
+
+export function Slider() {
+    return (
+        <div className="slider">
+            <h1>SLIDER</h1>
+        </div>
+    )
+}
--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,12 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import { Menu } from './Menu.jsx'
+import { Slider } from './Slider.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <>
     <App />
+    <Slider />   
   </>,
 )
