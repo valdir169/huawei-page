5a55abb0f7da6a700b2cbee67492df8c4c16e88c
Valdir Contreras <contrerasvaldir06.gmail.com>
2023-07-05 09:12:58 -0500

    update huawei-page

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,13 @@ import { Products } from "./Products"
 import { Slider } from "./components/Slider"
 import { products } from './json/products.json'
 import './App.css'
+import { SliderDos } from "./components/Slider2"
 
 export default function App() {
   return (
     <div className='App'>
       <Slider />
+      <SliderDos />
       <Products products={products} />
     </div>
   )
---
+++ src/components/Slider2.jsx
@@ -0,0 +1,22 @@
+import './sliderDos.css'
+
+export function SliderDos() {
+    return (
+        <div className="sliderDos">
+            <div className="sliderDos-image">
+                <img src="https://consumer.huawei.com/content/dam/huawei-cbg-site/common/mkt/homepage/new-homepage/new/nova-10-prokv.jpg" alt="" />
+            </div>
+
+            <div className="sliderDos-container">
+                <h3 className="sliderDos-header">HUAWEI FreeBuds 5</h3>
+                <p>Diseño curvo para un ajuste óptimo | Driver ultra magnético con graves poderosos | Hasta 30 hrs de reproducción de música</p>
+                <strong>Desde $ - 550</strong>
+
+                <div className="sliderDos-button">
+                    <button>Conoce Más</button>
+                    <button>Comprar</button>
+                </div>
+            </div>
+        </div>
+    )
+}
